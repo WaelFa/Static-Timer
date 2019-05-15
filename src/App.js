a62243c1aedf04b0c9f5417a0a4975f0ba2d21ff
WaelFa <wael.fadhel93@gmail.com>
2019-05-16 00:37:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import StaticTimer from './static-timer'
+
+function App() {
+  return (
+    <div className="App">
+     <StaticTimer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/static-timer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function StaticTimer() {
+  return (
+    <div className="StaticTimer">
+      <div className="Time">00:14:16</div>
+      <div className="Tags">Hour &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp; Minute &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp; Second</div>
+    </div>
+  );
+}
+export default StaticTimer;
